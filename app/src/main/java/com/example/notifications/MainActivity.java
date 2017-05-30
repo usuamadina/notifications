b8cd8c0c53b482351df14923ce77260468092087
Usua Madina <u.madina.i@gmail.com>
2017-05-30 19:26:47 +0200

    Notifications with specific wear actions

--- app/src/main/java/com/example/notifications/MainActivity.java
+++ app/src/main/java/com/example/notifications/MainActivity.java
@@ -11,6 +11,9 @@ import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -21,17 +24,27 @@ public class MainActivity extends AppCompatActivity {
         wearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mapIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=playa+de+la+concha"));
-                PendingIntent pendingMapIntent = PendingIntent.getActivity(MainActivity.this, 0, mapIntent,0);
-                Intent callIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("tel:555123456"));
-                PendingIntent callPendingIntent = PendingIntent.getActivity(MainActivity.this, 0 , callIntent,0);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=playa+de+la+concha"));
+                PendingIntent pendingMapIntent = PendingIntent.getActivity(MainActivity.this, 0, mapIntent, 0);
+
+                Intent callIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:555123456"));
+                PendingIntent callPendingIntent = PendingIntent.getActivity(MainActivity.this, 0, callIntent, 0);
+
                 int notificationId = 001;
+
+                NotificationCompat.Action action = new NotificationCompat.Action.Builder(R.mipmap.ic_action_call, "Wear Call", callPendingIntent).build();
+
+                List<NotificationCompat.Action> actionList = new ArrayList<NotificationCompat.Action>();
+                actionList.add(action);
+                actionList.add(new NotificationCompat.Action(R.mipmap.ic_action_locate, "Show Map", pendingMapIntent));
+
                 Notification notification = new NotificationCompat.Builder(MainActivity.this)
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle("Tittle")
                         .setContentText("Android Wear Notification")
                         .setContentIntent(pendingMapIntent)
-                        .addAction(R.mipmap.ic_action_call,"Call",callPendingIntent)
+                        .addAction(R.mipmap.ic_action_call, "Call", callPendingIntent)
+                        .extend(new NotificationCompat.WearableExtender().addActions(actionList))
                         .build();
                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
                 notificationManager.notify(notificationId, notification);
